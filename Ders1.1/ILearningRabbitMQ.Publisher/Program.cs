144a8cff3c0a3e93df2be60f6a37307a38492049
Alperen Kolat <73352908+alperenkolat@users.noreply.github.com>
2023-03-18 21:48:58 +0300

    add : added ders 1

---
+++ Ders1.1/ILearningRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+
+var factory =new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message{x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine(message);
+}
+);
+Console.ReadLine();
+
+
+
+    
---
+++ Ders1.1/ILearningRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+//channel.QueueDeclare("hello-queue", true, false, false);
+channel.BasicQos(0, 5, false);
+var consumer =new EventingBasicConsumer(channel);   
+channel.BasicConsume("hello-queue",false,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1000);
+    Console.WriteLine(message);
+    channel.BasicAck(e.DeliveryTag,false);
+};
+
+Console.ReadLine();
