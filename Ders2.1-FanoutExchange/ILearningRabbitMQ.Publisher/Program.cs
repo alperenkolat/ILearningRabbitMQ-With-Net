0260fd3d03ef8f97cd6768d84e66c22bb45de881
Alperen Kolat <73352908+alperenkolat@users.noreply.github.com>
2023-03-20 13:52:19 +0300

    feat: added fanout Exchange method

---
+++ Ders2.1-FanoutExchange/ILearningRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+
+var factory =new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+
+channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);
+
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message{x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("logs-fanout", "", null, messageBody);
+    Console.WriteLine(message);
+}
+);
+Console.ReadLine();
+
+
+
+    
---
+++ Ders2.1-FanoutExchange/ILearningRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+
+var randomQueueName=channel.QueueDeclare().QueueName;
+
+channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+channel.BasicQos(0, 1, false);
+var consumer =new EventingBasicConsumer(channel);   
+channel.BasicConsume(randomQueueName, false,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1000);
+    Console.WriteLine(message);
+    channel.BasicAck(e.DeliveryTag,false);
+};
+
+Console.ReadLine();
