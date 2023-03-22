dc851bbe4f348f879c6fbaf7c72cfb300a6b4e90
Alperen Kolat <73352908+alperenkolat@users.noreply.github.com>
2023-03-22 13:17:11 +0300

    feat: added direct exchange method

---
+++ Ders2.2-DirectExchange/ILearningRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ILearningRabbitMQ.Publisher;
+using RabbitMQ.Client;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Channels;
+
+
+
+
+        Console.WriteLine("Hello, World!");
+
+        var factory =new ConnectionFactory();
+        factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+        //channel.QueueDeclare("hello-queue", true, false, false);
+
+
+        channel.ExchangeDeclare("logs-direct",durable:true,type:ExchangeType.Direct);
+
+        Enum.GetNames(typeof(LogNames)).ToList().ForEach(x => {
+            var routeKey = $"route-{x}";
+            var queueName = $"direct-queue-{x}";
+            channel.QueueDeclare(queueName,true,false,false);
+            channel.QueueBind(queueName, "logs-direct", routeKey,null);
+
+            });
+
+
+        Enumerable.Range(1, 50).ToList().ForEach(x =>
+        {
+            LogNames log = (LogNames)new Random().Next(1, 5);
+            string message = $"Message-{log}";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            var routeKey = $"route-{log}";
+
+            channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+            Console.WriteLine(message);
+        }
+        );
+        Console.ReadLine();
+
+
+
---
+++ Ders2.2-DirectExchange/ILearningRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+
+channel.BasicQos(0, 1, false);
+var consumer =new EventingBasicConsumer(channel);
+
+var queueName = "direct-queue-Critical";
+    channel.BasicConsume(queueName, false,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1000);
+    Console.WriteLine(message);
+    File.AppendAllText(queueName+".txt", message+"\n");  //!does not support running multiple times
+    channel.BasicAck(e.DeliveryTag,false);
+};
+
+Console.ReadLine();
