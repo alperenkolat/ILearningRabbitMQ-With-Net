272eef303265db27c17d65b78d9732ea7704dfc6
Alperen Kolat <73352908+alperenkolat@users.noreply.github.com>
2023-03-23 12:49:31 +0300

    feat: added header exchange method

---
+++ Ders2.4-HeaderExchange/ILearningRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ILearningRabbitMQ.Publisher;
+using RabbitMQ.Client;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Channels;
+
+
+
+
+Console.WriteLine("Hello, World!");
+
+var factory =new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+var properties = channel.CreateBasicProperties();
+properties.Headers=headers;
+
+channel.BasicPublish("header-exchange",string.Empty,properties,Encoding.UTF8.GetBytes("header message"));
+Console.WriteLine("message posted:");
+Console.ReadLine();
+
+
+
---
+++ Ders2.4-HeaderExchange/ILearningRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.ComponentModel.Design;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+
+channel.BasicQos(0, 1, false);
+var consumer =new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+
+headers.Add("x-match", "all");
+
+channel.QueueBind(queueName, "header-exchange", string.Empty,headers);
+channel.BasicConsume(queueName, false,consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1000);
+    Console.WriteLine(message);
+    channel.BasicAck(e.DeliveryTag,false);
+};
+
+Console.ReadLine();
